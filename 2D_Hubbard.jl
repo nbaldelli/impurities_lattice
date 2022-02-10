eb8da976ae2f933f44e47b82be44eaf833addca8
niccobal <nbaldelli@icfo.net>
2022-02-10 16:27:39 +0100

    Added currents for rung geometry

--- 2D_Hubbard.jl
+++ 2D_Hubbard.jl
@@ -1,6 +1,6 @@
-using LinearAlgebra, SparseArrays, Plots, Arpack, Combinatorics, ITensors, ITensorGPU
+using LinearAlgebra, SparseArrays, Plots, Arpack, Combinatorics, ITensors, ITensorGPU, MKL
 
-function NKron(sites,op1,op2,pos) 
+?function NKron(sites,op1,op2,pos) 
     #=
     Tensor product operator 
     returns tensor product of op1,op2 on sites pos,pos+1 and identity on remaining sites
@@ -23,7 +23,6 @@ function NKron(sites,op1,op2,pos)
     end
     return mat
 end
-
 function square_lattice_arr(Nx::Int, Ny::Int; kwargs...)
     yperiodic = get(kwargs, :yperiodic, false)
     yperiodic = yperiodic && (Ny > 2)
@@ -48,7 +47,7 @@ function square_lattice_arr(Nx::Int, Ny::Int; kwargs...)
     end
     return latt
   end
-
+end
 function hopping_bhubbard(labels,lattice)
     br_term=10e-6
     in_states_list=Vector{Int64}()
@@ -74,24 +73,63 @@ function hopping_bhubbard(labels,lattice)
     matr=sparse(in_states_list,out_states_list,kin_en)
     return matr.+matr'
 end
+function currents_MPO(t, lattice::Vector{LatticeBond}, sites,  α = 0) #Currents for ladder geometry with flux (this is the full MPO)
+    curr_lowleg = OpSum(); curr_highleg = OpSum(); curr_rung = OpSum()
 
-labels=base_construction(num_bos,Nx * Ny)
-lattice = square_lattice_arr(Nx, Ny; yperiodic=true)
+    for b in lattice
+        curr_lowleg += 1im*t*(b.x2-b.x1)*isodd(b.s1), "Adag", b.s2, "A", b.s1 #current on the bottom leg
+        curr_lowleg += -1im*t*(b.x2-b.x1)*isodd(b.s1), "Adag", b.s1, "A", b.s2
 
-#function main(; Nx=6, Ny=3, U=4.0, t=1.0)
+        curr_highleg += 1im*t*(b.x2-b.x1)*iseven(b.s1), "Adag", b.s2, "A", b.s1 #current on the top leg
+        curr_highleg += -1im*t*(b.x2-b.x1)*iseven(b.s1), "Adag", b.s1, "A", b.s2
+
+        curr_rung += 1im*t*(b.y2-b.y1)*exp(-2pi*1im*α*b.x1*(b.y2-b.y1)), "Adag", b.s1, "A", b.s2 #current on the rungs
+        curr_rung += 1im*t*(b.y2-b.y1)*exp(+2pi*1im*α*b.x1*(b.y2-b.y1)), "Adag", b.s2, "A", b.s1
+    end
+    return MPO(curr_lowleg, sites), MPO(curr_highleg, sites), MPO(curr_rung, sites)
+end
+
+function currents_from_correlation(t, lattice::Vector{LatticeBond}, C, Nx, Ny,  α = 0) #Currents for ladder geometry with flux (this is the full MPO)
+    curr_lowleg = zeros(ComplexF64,Nx); curr_highleg = zeros(ComplexF64,Nx); curr_rung = zeros(ComplexF64,Nx)
+    for b in lattice
+        curr_lowleg[Int(b.x1)] += 1im*t*(b.x2-b.x1)*isodd(b.s1)*C[b.s2, b.s1] #current on the bottom leg
+        curr_lowleg[Int(b.x1)] += -1im*t*(b.x2-b.x1)*isodd(b.s1)*C[b.s1, b.s2]
+
+        curr_highleg[Int(b.x1)] += 1im*t*(b.x2-b.x1)*iseven(b.s1)*C[b.s2, b.s1] #current on the top leg
+        curr_highleg[Int(b.x1)] += -1im*t*(b.x2-b.x1)*iseven(b.s1)*C[b.s1, b.s2]
+
+        curr_rung[Int(b.x1)] += 1im*t*(b.y2-b.y1)*exp(-2pi*1im*α*b.x1*(b.y2-b.y1))*C[b.s1,b.s2] #current on the rungs
+        curr_rung[Int(b.x1)] += -1im*t*(b.y2-b.y1)*exp(+2pi*1im*α*b.x1*(b.y2-b.y1))*C[b.s2,b.s1]
+    end
+    return curr_lowleg, curr_highleg, curr_rung
+end
+
+function entanglement_entropy(psi,b)
+    orthogonalize!(psi, b)
+    U,S,V = svd(psi[b], (linkind(psi, b-1), siteind(psi,b)))
+    p=0.0
+    SvN = 0.0
+    for n=1:dim(S, 1)
+        p = S[n,n]^2
+        SvN -= p * log(p)
+    end
+    return SvN
+end
+
+labels=base_construction(num_bos,Nx * Ny)   
+lattice = square_lattice_arr(Nx, Ny; yperiodic=false)
+
+#let #scaling up of magnetic flux
 ##
-#let
-
-let #scaling up of magnetic flux
     μ = 0.0; t = 1.0; U = 0.0; αtot = 1//4;
-    Nx = 20; Ny = 2 #Nx is the easy dimension, Ny the difficult
+    Nx = 19; Ny = 5 #Nx is the easy dimension, Ny the difficult
     N = Nx * Ny
     
     ν = 1//2 #filling
 
     Nϕ = αtot*(Nx-1)*(Ny-1) #number of fluxes
     @show num_bos = ν * Nϕ 
-    num_bos = Nx
+    #num_bos = 2
 
     #check increasing number of bosons if i get convergence (10 or 14) 
     #check that it is gapless (with correlation functions bdagger b)
@@ -100,63 +138,72 @@ let #scaling up of magnetic flux
 
     loc_hilb = 2 #local hilbert space
     sweeps = Sweeps(10)
-    setmaxdim!(sweeps, 10, 20, 40, 80, 160, 320)
+    setmaxdim!(sweeps, 10, 10, 20, 20, 40, 80, 80, 160, 320, 450)
     setcutoff!(sweeps, 1e-10)
-    setnoise!(sweeps, 1e-3, 1e-5, 1e-7, 0)
+    setnoise!(sweeps, 1e-3, 1e-5, 1e-7, 1e-9, 0)
     @show sweeps
 
     sites = siteinds("Boson", N; dim=loc_hilb, conserve_qns=true)
     lattice = square_lattice(Nx, Ny; yperiodic=false)
 
-    state = [isodd(n) ? "1" : "0" for n in 1:(N-0)]
+    state = [n<=num_bos ? "1" : "0" for n in 1:(N-0)]
     append!(state,fill("0",0))
     psi0 = randomMPS(sites, state)
-
-    function currents(lattice::Vector{LatticeBond})
         
-
-
-    for α in [0]
+    for α in [1/4]
 
         ampo = OpSum()
         for b in lattice
-        ampo += -t*exp(-2pi*1im*α*b.x1*(b.y2-b.y1)), "Adag", b.s1, "A", b.s2 #hopping with flux
-        ampo += -t*exp(+2pi*1im*α*b.x1*(b.y2-b.y1)), "Adag", b.s2, "A", b.s1
+            ampo += -t*exp(-2pi*1im*α*b.x1*(b.y2-b.y1)), "Adag", b.s1, "A", b.s2 #hopping with flux
+            ampo += -t*exp(+2pi*1im*α*b.x1*(b.y2-b.y1)), "Adag", b.s2, "A", b.s1
         end
         for n in 1:N
-        ampo += -μ, "N", n #chemical potential
-        ampo += U, "N", n, "N", n #on-site interaction
-        ampo += -U, "N", n 
+            ampo += -μ, "N", n #chemical potential
+            ampo += U, "N", n, "N", n #on-site interaction
+            ampo += -U, "N", n 
         end
 
         H = MPO(ampo, sites)
 
-        #legs and rung currents, this work only for ladders
-        j_par = OpSum()
-        for i in 1:N
-        j_par += 1im * t * (b.x2 - b.x1), "Adag", b.s1, "A", b.s2
-        j_par += 1im * t *
-
-        # Half filling
-        #state = [Int(N/2)<=n<=Int(N/2)+num_bos-1 ? "1" : "0" for n in 1:N]
-
         energy, psi = @time dmrg(H, psi0, sweeps, verbose=false);
-        C = real(correlation_matrix(psi, "Adag", "A"))
-        c = display(scatter(C[Int(Nx/2),Int(Nx//2):end]))
 
-        psih = H * psi
-        variance = @show dot(psih,psih) - energy^2
+        ent_entr = zeros(N-3)
+        for (ind, j) in enumerate((1:1:N)[2:end-2])
+            ent_entr[ind] = entanglement_entropy(psi, j)
+        end
 
-        aa = reshape(expect(psi, "N"),(Ny,Nx))
+        C = correlation_matrix(psi, "Adag", "A")
+        #c = display(scatter(C[Int(Nx/2), Int(Nx//2):end]))
+        jlow, jhigh, jrung = currents_from_correlation(t, lattice, C, Nx, Ny, α)
+        curr=round.(real(vcat(transpose(jlow),transpose(jrung),transpose(jhigh))),digits=9)
+
+        H2 = inner(H, psi, H, psi)
+        E = inner(psi, H, psi)
+        var = H2-E^2
+        @show var
+        
+        aa = reshape(expect(psi, "N"), (Ny, Nx))
         @show sum(aa)
         #var = @show expect(psi,H*H) - energy^2
 
         psi0 = deepcopy(psi)
 
+        a = plot(real(ent_entr))
+        title!(a,"ent. entr., loc_hilb=$loc_hilb, U=$U, α=$α")
+        display(a)
+
         p = heatmap([string(i) for i in 1:Nx], [string(j) for j in 1:Ny], aa, c = :heat)
-        title!(p,"loc_hilb=$loc_hilb, U=$U, α=$α")
+        title!(p,"dens, loc_hilb=$loc_hilb, U=$U, α=$α")
         vline!(p, [i for i in 1:Nx], c=:black)
         hline!(p, [j for j in 1:Ny], c=:black)
         display(p)
+
+        d = heatmap([string(i) for i in 1:Nx], [string(j) for j in 1:(Ny+1)], curr, c = :heat)
+        title!(d,"current, loc_hilb=$loc_hilb, U=$U, α=$α")
+        vline!(d, [i for i in 1:Nx], c=:black)
+        hline!(d, [j for j in 1:Ny], c=:black)
+        display(d)
+
     end
-end
+
+#end
